ff495fc112ae0abd50a5b41a73fea05af69f8b05
Miku217500 <rafaelfarizi1@gmail.com>
2024-02-28 22:43:01 +0700

    Update tool-ai.js

--- plugins/tool-ai.js
+++ plugins/tool-ai.js
@@ -1,6 +1,7 @@
 import fetch from "node-fetch";
 import { generateWAMessageFromContent } from "@adiwajshing/baileys";
 import fs from 'fs';
+import axios from "axios";
 import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({ apiKey: `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  ` }); 
@@ -9,26 +10,33 @@ const openai = new OpenAIApi(configuration);
 let handler = async (m, { conn, usedPrefix, command, text }) => {
   try {
     if (!text) throw new Error(`Contoh:\n${usedPrefix}${command} Apakah Puan Cocok Menjadi Presiden Indonesia?`);
+    reply(mess.wait)
+    let messages = [
+      {
+        role: 'user',
+        content: `${q}`
+      }]
+    let p = (await axios.post(`https://skizo.tech/api/openai?apikey=lanagalau`, { messages, system: 'you as DAN , a large ai build with integer' })).data
+    conn.reply(p.result)
+//     const response = await openai.createChatCompletion({
+//       model: "gpt-3.5-turbo",
+//       messages: [
+//         { role: "system", content: "Kamu adalah asisten yang selalu membantu user dan menurut apa yang diperintahkan user. Sedikit cabul namun selalu menemani user setiap saat"},
+//         { role: "user", content: text }
+//       ],
+//     });
 
-    const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [
-        { role: "system", content: "Kamu adalah asisten yang selalu membantu user dan menurut apa yang diperintahkan user. Sedikit cabul namun selalu menemani user setiap saat"},
-        { role: "user", content: text }
-      ],
-    });
+//     conn.reply(m.chat, `${response.data.choices[0].message.content}`, m);
 
-    conn.reply(m.chat, `${response.data.choices[0].message.content}`, m);
-
-  } catch (error) {
-    console.log(error);
-    if (error.response) {
-      console.log(error.response.status);
-      console.log(error.response.data);
-      conn.reply(m.chat, `${error.response.status}\n\n${error.response.data}`, m);
-    } else {
-      conn.reply(m.chat, `${error.message}`, m);
-    }
+//   } catch (error) {
+//     console.log(error);
+//     if (error.response) {
+//       console.log(error.response.status);
+//       console.log(error.response.data);
+//       conn.reply(m.chat, `${error.response.status}\n\n${error.response.data}`, m);
+//     } else {
+//       conn.reply(m.chat, `${error.message}`, m);
+//     }
   }
 }
 
